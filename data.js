befd2dde217005d1d20da4b3a013232b5978f1aa
unknown <rmtestard@gmail.com>
2020-04-18 11:48:08 +0200

    I have added data

---
+++ data.js
@@ -0,0 +1,189 @@
+module.exports = [
+    [
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191111182325.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"0e966866d53fdc7d7dedcd87b2f994d2"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object]
+        },
+        {
+            Key: 'values/archive/input/RAP_EH260499_20191217184250.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"09f196634390e4adff57b6376f802e56"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/input/RAP_EH260499_20191227174751.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"eb0c0277983a1043a75f1310dd880bb9"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191230174739.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"724eee70f3606c1f699ad650b8338894"',
+            Size: 6432,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20200102174741.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"fbbb24627fa5f6eac1199e75e3557cf2"',
+            Size: 5628,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        }
+    ],
+    [
+        {
+            Key: 'values/archive/input/RAP_EH260499_20191111182325.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"0e966866d53fdc7d7dedcd87b2f994d2"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/input/RAP_EH260499_20191217184250.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"09f196634390e4adff57b6376f802e56"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/input/RAP_EH260499_20191227174751.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"eb0c0277983a1043a75f1310dd880bb9"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191230174739.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"724eee70f3606c1f699ad650b8338894"',
+            Size: 6432,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20200102174741.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"fbbb24627fa5f6eac1199e75e3557cf2"',
+            Size: 5628,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        }
+    ],
+    [
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191111182325.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"0e966866d53fdc7d7dedcd87b2f994d2"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191217184250.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"09f196634390e4adff57b6376f802e56"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191227174751.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"eb0c0277983a1043a75f1310dd880bb9"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191230174739.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"724eee70f3606c1f699ad650b8338894"',
+            Size: 6432,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20200102174741.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"fbbb24627fa5f6eac1199e75e3557cf2"',
+            Size: 5628,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        }
+    ],
+    [
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191111182325.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"0e966866d53fdc7d7dedcd87b2f994d2"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191217184250.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"09f196634390e4adff57b6376f802e56"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191227174751.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"eb0c0277983a1043a75f1310dd880bb9"',
+            Size: 6834,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20191230174739.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"724eee70f3606c1f699ad650b8338894"',
+            Size: 6432,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        },
+        {
+            Key: 'values/archive/output/RAP_EH260499_20200102174741.TXT',
+            LastModified: '2020-01-02T16:47:59.617Z',
+            ETag: '"fbbb24627fa5f6eac1199e75e3557cf2"',
+            Size: 5628,
+            StorageClass: 'STANDARD',
+            Owner: [Object
+            ]
+        }
+    ]
+]
